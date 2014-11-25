415a99e69d60e9428077f00819187d60b11b848a
Svpernova09 <joe@joeferguson.me>
2014-11-25 07:51:21 -0600

    Actually add the testing env file >.<

---
+++ .env.testing.php
@@ -0,0 +1,8 @@
+<?php return array (
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'homestead',
+    'DB_USERNAME' => 'travis',
+    'DB_PASSWORD' => '',
+    'MANDRILL_SECRET' => '',
+    'DEBUG' => true
+);
