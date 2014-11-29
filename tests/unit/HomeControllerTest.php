d16136332085ef0759a09a48d9f2ed0e1fbc7f4f
Svpernova09 <joe@joeferguson.me>
2014-11-29 12:46:53 -0600

    add test for index method to make sure response is ok

---
+++ tests/unit/HomeControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class HomeControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $response = $this->action('GET', 'HomeController@index');
+        $this->assertResponseOk();
+    }
+}
+
