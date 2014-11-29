ad0cd19b149a50f0de32d6614fe613d3c0cd8151
Svpernova09 <joe@joeferguson.me>
2014-11-28 19:46:56 -0600

    Add sponsor table seeder. Should look at a better way to handle sponsors that have multiple years

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		 $this->call('YearsTableSeeder');
+		$this->call('YearsTableSeeder');
+		$this->call('SponsorTableSeeder');
 	}
 
 }
