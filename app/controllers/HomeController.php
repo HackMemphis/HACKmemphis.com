488b5587c0f0e414cbd6d864dc233d771d03b267
Svpernova09 <joe@joeferguson.me>
2014-11-30 16:36:51 -0600

    Pull sponsors by year and pass to index view

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,22 +2,27 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
+	protected $sponsor;
+	protected $year;
+
+	public function __construct(Sponsor $sponsor, Year $year)
+	{
+		$this->sponsor = $sponsor;
+		$this->year = $year;
+	}
 
 	public function index()
 	{
-		return View::make('index');
+		// Get 2014 Sponsors
+		$year2014 = $this->year->where('year', '2014')->first();
+		$sponsors['2014'] = Year::find($year2014->id)->sponsors;
+
+		// Get 2013 Sponsors
+		$year2013 = $this->year->where('year', '2013')->first();
+		$sponsors['2013'] = Year::find($year2013->id)->sponsors;
+
+		return View::make('index')
+			->with('sponsors', $sponsors);
 	}
 
 }
