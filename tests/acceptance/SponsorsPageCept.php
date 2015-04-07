d642746d0566be3f3e7977a4429c1ed2ef9ca277
Joe Ferguson <joe@joeferguson.me>
2015-04-06 19:50:01 -0500

    update tests with new sponsors section and page

--- tests/acceptance/SponsorsPageCept.php
+++ tests/acceptance/SponsorsPageCept.php
@@ -3,5 +3,6 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure sponsors section displays');
 $I->amOnPage('/sponsors');
 $I->see('#sponsors');
-$I->see('2015 Sponsors', '.section-title');
+$I->see('2014 Sponsors', '.section-title');
+$I->see('2013 Sponsors', '.section-title');
 $I->dontSee('whoops');
